8c3034df12bc935f872672094907224198433219
ViberOptic <muhamad0azka@gmail.com>
2025-12-11 07:11:38 +0700

    New

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import Navbar from "@/components/Navbar";
 import SplashScreen from "@/components/shared/SplashScreen";
 
 // Import Sections
-import HomePage from "@/components/sections/HomePage";
-import ProjectsPage from "@/components/sections/ProjectsPage";
-import AboutPages from "@/components/sections/AboutPages";
-import ContactPages from "@/components/sections/ContactPages";
+import HomePage from "@/components/sections/Home";
+import ProjectsPage from "@/components/sections/Projects";
+import AboutPages from "@/components/sections/About";
+import ContactPages from "@/components/sections/Contact";
 
 export default function Home() {
   const [showSplash, setShowSplash] = useState(true);
